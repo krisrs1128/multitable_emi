bcc2dc31c67775d74a323f258614489fa8f7ed00
Kris Sankaran <krissankaran@DN525jh6.SUNet>
2015-10-26 15:51:33 -0700

    Update mean predictor vignette to use the general pipeline

--- emi/R/mean_predictor.R
+++ emi/R/mean_predictor.R
@@ -3,23 +3,41 @@
 # Simple predictors based on the mean
 ################################################################################
 
-# mean-predictor ---------------------------------------------------------------
-#' @title Predict using the mean of a group
-#' @param train The training data, with a column for "type" and a column for
-#' true ratings.
-#' @param test The test data, with a column for "type", whose ratings we want to
-#' predict.
-#' @param type The column to take averages over. Defaults to user.
-#' @return pred_means The predicted ratings for the test data.
-#' @importFrom magrittr %>%
+# mean-model -------------------------------------------------------------------
+#' @title "Train" a model that predicts the means within groups.
+#' @param data_list A list containing the following elements, paralleling the
+#' input provided by Kaggle,\cr
+#'   train: A data.table giving the artist, track, user, rating, and time info. \cr
+#'   words: A data.table giving word indicators for artist-user pairs. \cr
+#'   users: A data.table givin gsurvey results for each user. \cr
+#' We don't ever actually use words or users in this model. This convention is
+#' enforced for simplicity only.
+#' @param train_opts A list with an element called type, specifying the group to
+#' average over.
+#' @return A list with the following elements \cr
+#'    cur_means: The means within groups for this model.
+#'    type: The type of group over which we calculated means.
+#'    global_mean: The mean over all ratings.
 #' @importFrom dplyr group_by_ summarise
 #' @export
-mean_predictor <- function(train, test, type = "User") {
-  cur_summary <- train %>%
+mean_model <- function(data_list, train_opts = list(type = "User")) {
+  type <- train_opts$type
+  cur_summary <- data_list$train %>%
     group_by_(type) %>%
     summarise(mean = mean(Rating))
-  cur_means <- setNames(cur_summary$mean, unlist(cur_summary[, type]))
-  pred_means <- cur_means[test[, type]]
-  pred_means[is.na(pred_means)] <- mean(train$Rating)
+  cur_means <- setNames(cur_summary$mean, unlist(cur_summary[, type, with = F]))
+  global_mean <- mean(data_list$train$Rating, na.rm = T)
+  return (list(cur_means = cur_means, type = type, global_mean = global_mean))
+}
+
+#' @title "Predict" using means within groups
+#' @param mean_model The output of a call to mean_model.
+#' @param data_list A list in the same form as in the input to mean_model, but
+#' with only the test cases.
+#' @return Predictions on the test cases.
+#' @export
+mean_pred <- function(model, data_list) {
+  pred_means <- model$cur_means[unlist(data_list$train[, model$type, with = F])]
+  pred_means[is.na(pred_means)] <- model$global_mean
   return (pred_means)
 }
--- emi/vignettes/mean_baselines.R
+++ emi/vignettes/mean_baselines.R
@@ -16,6 +16,8 @@ library("caret")
 # ---- get-data ----
 data(train)
 data(test)
+data(users)
+data(words)
 
 # only using 1/5 of users in this experiment
 subset_users <- sample(as.character(unique(train$User)), 1e3)
@@ -29,51 +31,22 @@ length(intersect(unique(train$User), unique(test$User)))
 length(setdiff(unique(train$User), unique(test$User)))
 length(setdiff(unique(test$User), unique(train$User)))
 
-length(intersect(unique(cur_train$User), unique(cur_test$User)))
-length(setdiff(unique(cur_train$User), unique(cur_test$User)))
-length(setdiff(unique(cur_test$User), unique(cur_train$User)))
-
-rmses <- matrix(nrow = length(test_ix), ncol = 6)
-colnames(rmses) <- c("mean", "user", "artist", "time", "svd", "track")
-
-for(k in seq_along(test_ix)) {
-  cat(sprintf("Starting cv fold %g \n", k))
-  cur_train <- train[-test_ix[[k]], ]
-  cur_test <- train[test_ix[[k]], ]
-
-  # make predictions
-  y_hat_mean <- rep(mean(cur_train$Rating), nrow(cur_test))
-  y_hat_user <- mean_predictor(cur_train, cur_test)
-  y_hat_artist <- mean_predictor(cur_train, cur_test, "Artist")
-  y_hat_time <- mean_predictor(cur_train, cur_test, "Time")
-  y_hat_track <- mean_predictor(cur_train, cur_test, "Track")
-  y_hat_svd <- svd_predictor(cur_train, cur_test, r = 4, alpha = 0.9)$y_hat
-
-  # Get RMSEs
-  rmses[k, "mean"] <- RMSE(y_hat_mean, cur_test$Rating)
-  rmses[k, "artist"] <- RMSE(y_hat_artist, cur_test$Rating)
-  rmses[k, "user"] <- RMSE(y_hat_user, cur_test$Rating)
-  rmses[k, "time"] <- RMSE(y_hat_time, cur_test$Rating)
-  rmses[k, "track"] <- RMSE(y_hat_track, cur_test$Rating)
-  rmses[k, "svd"] <- RMSE(y_hat_svd, cur_test$Rating)
-}
+rmses <- list()
+rmses$track <- evaluate(list(train = train, users = users, words = words),
+                        mean_model,
+                        list(type = "Track"),
+                        mean_pred, n_rep = 10)$errs
+rmses$artist <- evaluate(list(train = train, users = users, words = words),
+                        mean_model,
+                        list(type = "Artist"),
+                        mean_pred, n_rep = 10)$errs
+rmses$user <- evaluate(list(train = train, users = users, words = words),
+                        mean_model,
+                        list(type = "User"),
+                        mean_pred, n_rep = 10)$errs
 
 # We actually do better just predicting the overall mean in the training data
 mrmses <- melt(rmses)
 ggplot(mrmses) +
   geom_histogram(aes(x = value), binwidth = .05) +
-  facet_grid(Var2 ~ .)
-
-ggplot(mrmses) +
-  geom_histogram(aes(x = value), binwidth = .05) +
-  facet_grid(Var2 ~ .) +
-  xlim(21, 24)
-
-# ---- svd-ideas ----
-svd_res <- svd_predictor(cur_train, cur_test, r = 10)
-plot(svd_res$svd$d)
-plot(svd_res$svd$u[, 1:2])
-
-ggplot(data.frame(truth = cur_test$Rating, pred = svd_res$y_hat)) +
-  geom_point(aes(x = truth, y = pred), alpha = 0.25, size = .7) +
-  coord_fixed()
+  facet_grid(L1 ~ .)
