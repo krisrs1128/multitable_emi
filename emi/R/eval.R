a2dfabb3ee7bbf661e4efaa49dff026c545e11e1
Kris Sankaran <krissankaran@DN525jh6.SUNet>
2015-10-26 14:50:30 -0700

    Don't bother saving fold ix

--- emi/R/eval.R
+++ emi/R/eval.R
@@ -3,22 +3,23 @@
 # Functions for evaluating performance
 ################################################################################
 
+# metrics ----------------------------------------------------------------------
 #' @title Calculate the root mean squared error
 #' @export
 RMSE <- function(y, y_hat) {
   sqrt(mean((y - y_hat) ^ 2))
 }
 
+# get-folds --------------------------------------------------------------------
 #' @title Get CV Folds for evaluation
 #' @importFrom caret createFolds
 #' @importFrom devtools use_data
 #' @export
-get_folds <- function(data = NULL, file = NULL, n = 188690, K = 5, n_rep = 20) {
-  if(is.null(file)) {
-    test_ix <- replicate(n_rep, createFolds(seq_len(nrow(train)), k = K), simplify = T)
-    use_data(test_ix, overwrite = TRUE)
-  } else {
-    suppressWarnings(data(get(file)))
-  }
-  return (test_ix)
+get_folds <- function(n = 188690, K = 5, n_rep = 20) {
+  replicate(n_rep, createFolds(seq_len(nrow(train)), k = K), simplify = T)
+}
+
+# evaluation -------------------------------------------------------------------
+evaluate_model <- function(data_list, train_fun, pred_fun) {
+
 }
