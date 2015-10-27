175ba19560f28b6e8e5c215e564fbdc5c47bbe8f
Kris Sankaran <krissankaran@DN525jh6.SUNet>
2015-10-26 19:18:48 -0700

    Some misc. experiments around mds idea

--- emi/vignettes/mds_predict.R
+++ emi/vignettes/mds_predict.R
@@ -7,6 +7,7 @@
 library("plyr")
 library("dplyr")
 library("emi")
+library("reshape2")
 library("ggplot2")
 library("proxy")
 
@@ -53,3 +54,61 @@ ggplot(words_mds) +
 ggplot(words_mds) +
   geom_point(aes(x = V1, y = V2)) +
   facet_wrap(~Artist)
+
+# calculating distances between artists using words
+data(words)
+words$User <- as.factor(words$User)
+words$Artist <- as.factor(words$Artist)
+artist_means <- words %>%
+  group_by(Artist) %>%
+  summarise_each(funs(mean(., na.rm = T)))
+
+D_artists <- dist(artist_means[, 3:ncol(artist_means), with = F])
+artist_mds <- data.frame(Artist = artist_means$Artist, cmdscale(D_artists))
+
+artist_ratings <- train %>%
+  group_by(Artist) %>%
+  summarise(mean = mean(Rating))
+
+artist_mds <- merge(artist_mds, artist_ratings)
+ggplot(artist_mds) +
+  geom_point(aes(x = X1, y = X2, col = mean))
+
+# random forest proximity
+train <- train[1:10000, ]
+X <- merge(users, words, by = "User", all = T)
+X <- merge(train, X, by = c("User", "Artist"), all.x = T)
+
+x_num <- as.matrix(X[, 6:ncol(X), with = F])
+x_num <- scale(x_num, center = F, scale = apply(x_num, 2, max, na.rm = T))
+library("imputation")
+x_hat <- SVDImpute(x_num, 10)
+
+x_hat <- x_hat$x
+corX <- cor(x_hat)
+corX[upper.tri(corX)] <- NA
+diag(corX) <- NA
+head(arrange(melt(corX), desc(abs(value))))
+
+x_fac <- expand_factors(X[, c("Track", "Time"), with = F])
+x <- cbind(x_fac, x_hat)
+y <- X$Rating
+
+library("caret")
+
+svdX <- svd(x)
+k <- 5
+z <- svdX$u[, seq_len(k)] %*% diag(svdX$d[seq_len(k)]) # pc regression
+
+glmnet_model <- train(x = x, y = y, method = "glmnet", trControl = trainControl(verbose = T))
+gbm_model <- train(x = z, y = y, method = "gbm", trControl = trainControl(verbose = T))
+
+glmnet_model
+gbm_model
+plot(y, predict(glmnet_model), asp = 1)
+plot(y, predict(glmnet_model), asp = 1)
+
+rpart_model <- train(x = z, y = y, method = "rpart", trControl = trainControl(verbose = T))
+plot(rpart_model$finalModel)
+
+
