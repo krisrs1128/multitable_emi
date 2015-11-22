833637591b9559b223d9d2c30c7ea01d6b2a91a5
Kris Sankaran <sankaran.kris@gmail.com>
2015-11-22 14:51:22 -0800

    sketch gradient updates for lf with covariates

--- emi/R/lf_gd_cov.R
+++ emi/R/lf_gd_cov.R
@@ -5,7 +5,8 @@
 
 #' @title Regular gradient descent for latent factor model with covariates
 #' @export
-lf_gd_cov <- function(X, Z, k_factors = 3, lambas = c(1, 1, 1), n_iter = 100) {
+lf_gd_cov <- function(X, Z, k_factors = 3, lambas = c(1, 1, 1), n_iter = 100,
+                      gamma = 0.1) {
   m <- nrow(X)
   n <- ncol(X)
   l <- dim(Z)[3]
@@ -18,13 +19,39 @@ lf_gd_cov <- function(X, Z, k_factors = 3, lambas = c(1, 1, 1), n_iter = 100) {
   for(cur_iter in seq_len(n_iter)) {
     # update user factors, movie factors, the regression coefficients
     for(i in seq_len(m)) {
-      P[i, ] <- update_p(X[i, ], Z[i,, ], P[i, ], Q, beta, lambdas[1])
+      P[i, ] <- update_p(X[i, ], Z[i,, ], P[i, ], Q, beta, lambdas[1], gamma)
     }
     for(j in seq_len(n)) {
-      Q[j, ] <- update_q(X[, j], Z[, j,], P, Q[j, ], beta, lambdas[2])
+      Q[j, ] <- update_q(X[, j], Z[, j,], P, Q[j, ], beta, lambdas[2], gamma)
     }
     beta <- update_beta(X, P, Q, beta, lambdas[3])
   }
 
-  return (list(P = P, Q = Q, beta = beta)
+  return (list(P = P, Q = Q, beta = beta))
+}
+
+update_factor <- function(x, z, update_factor, other_factor, lambda, gamma) {
+  obs_ix <- !is.na(x)
+  other_factor_obs <- other_factor[obs_ix, ]
+  resid <- x[obs_ix] - t(other_factor_obs) %*% udpate_factor - z[other_factor_obs, ] %*% beta
+  (1 - lambda) * update_factor + lambda * gamma * t(other_factor_obs) %*% resid
+}
+
+update_q <- function(x_j, z_j, P, q_j, beta, lambda, gamma) {
+  update_factor(x_j, z_j, q_j, P, beta, lambda, gamma)
+}
+
+udpate_p <- function(x_i, z_i, p_i, Q, beta, lambda, gamma) {
+  update_factor(x_i, z_j, p_i, Q, beta, lambda, gamma)
+}
+
+
+update_beta <- function(X, P, Q, beta, lambda, gamma) {
+  obs_ix <- is.na(X)
+  resid <- X - P %*% t(Q) - Z %*% beta
+  beta_grad <- vector(length = length(beta))
+  for(l in seq_len(beta_grad)) {
+    beta_grad[l] <- sum(resid[obs_ix] * Z[,, l][obs_ix])
+  }
+  (1 - lambda) * beta + lambda * gamma * beta_grad
 }
